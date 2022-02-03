4d9f5e525aa5504460d5cdae75e6fb59aed33293
MizunagiKB <mizukb@LIVE.JP>
2022-02-03 09:03:20 +0900

    API 220118 to 220131
    There are no changes on the client code side.

--- PlayFabSDK/PlayFabSettings.gd
+++ PlayFabSDK/PlayFabSettings.gd
@@ -62,7 +62,7 @@ class InternalSettings:
     This is automatically populated by any PlayFabClientApi.Login method.
     """
     var ClientSessionTicket = null
-    var SdkVersionString = "GDScriptSdk-0.0.220118"
+    var SdkVersionString = "GDScriptSdk-0.0.220131"
     var RequestGetParams = {
         "sdk": SdkVersionString
     }
